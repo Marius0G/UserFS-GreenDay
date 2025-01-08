7375ed8023956c7574217867d8d00ff34b066d8b
Bogdan-ca <bogdan-andrei.caraeane@s.unibuc.ro>
2025-01-08 15:07:21 +0200

    Creeare loop-ului in care se realizeaza operatiunile

--- test.sh
+++ test.sh
@@ -4,3 +4,12 @@ ROOT_DIR="./utilizatori_activi"
 
 # Se face folderul daca nu exista (-p ca sa nu dea erori daca exista deja)
 mkdir -p "$ROOT_DIR"
+
+#loop infinit care se repeta la 30 de secunde
+while true; do
+
+
+
+ 
+    sleep 30
+done
